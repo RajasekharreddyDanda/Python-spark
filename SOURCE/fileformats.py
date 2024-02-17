defd3fe2ac7f61724af6028da0b156c30cdc1eb9
dandarajreddy <42580454+RajasekharreddyDanda@users.noreply.github.com>
2024-02-17 22:57:00 +0530

    ----

--- SOURCE/fileformats.py
+++ SOURCE/fileformats.py
@@ -17,9 +17,23 @@ with open(file_path, "r") as file:
 #  Convert the rows into a Pandas DataFrame
 dataframe = pd.DataFrame(rows)
 print(dataframe)
-columns_to_sum = dataframe.columns[1:]
-print(columns_to_sum)
-print(dataframe)
 
+#print total num of all columns
+num_cols = dataframe.shape[1]
+print("Number of Columns : ", num_cols)
 
+# Get column names from first row of the dataframe (assuming it is present) 
+colnames = dataframe.iloc[0]       
+# Remove this row from the dataframe
+dataframe = dataframe[1:]                    
+# Set the column names to be used in the dataframe
+dataframe.columns = colnames 
+# Print number of rows and columns in the dataframe
+print("Number of Rows : ", dataframe.shape[0])
+
+# total flowers  given by women aged between 25-34 years old
+                                                   
+flowers_women_aged_25_34 = dataframe[(dataframe['Age'] >= 25) & (dataframe['Age'] <= 34)]['Flowers'].sum()
+print('Total Flowers Given By Women Aged Between 25 And 34 Years Old : ', flowers_women_aged_25_34)
+# total number of men who gave flowers to women aged between 25-34 years old
 
